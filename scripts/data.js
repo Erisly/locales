0dc96c634e787b0d0f51c8971e749fa28148a913
PikaDude <mail@pikadude.me>
2019-04-04 20:39:00 +1100

    data.js

--- scripts/data.js
+++ scripts/data.js
@@ -37,7 +37,7 @@ request('https://translate.pikagirl.me/api/components/PikaGirl/bot/statistics/',
                         name: result.name,
                         translated: percentage
                     }
-    
+
                     callback();
                 });
             });
@@ -47,28 +47,50 @@ request('https://translate.pikagirl.me/api/components/PikaGirl/bot/statistics/',
             console.log("Reading data.json");
             fs.readFile('./data.json', 'utf-8', (err, _data) => {
                 if (err) throw new Error(err);
-    
+
                 console.log("Merging statistic data");
                 var dataJSON = JSON.parse(_data);
-    
+
                 async.forEachOf(dataJSON, (value, key, callback) => {
                     if (finalJSON[key] != null) {
                         if (value.flag != null) finalJSON[key].flag = value.flag;
                         else finalJSON[key].flag = "❓";
                     }
-    
+
                     callback();
                 }, function (err) {
                     if (err) throw new Error(err);
 
+                    orderKeys(finalJSON);
+
                     console.log("Writing statistic data");
                     fs.writeFile('./data.json', JSON.stringify(finalJSON, null, "\t"), async err => {
                         if (err) throw new Error(err);
-        
+
                         console.log("Wrote statistic data");
                     });
                 });
-            });    
+            });
         });
     } else throw new Error('Request responded with status code: ' + res.statusCode);
 });
+
+function orderKeys(obj) {
+
+    var keys = Object.keys(obj).sort(function keyOrder(k1, k2) {
+        if (k1 < k2) return -1;
+        else if (k1 > k2) return +1;
+        else return 0;
+    });
+
+    var i, after = {};
+    for (i = 0; i < keys.length; i++) {
+        after[keys[i]] = obj[keys[i]];
+        delete obj[keys[i]];
+    }
+
+    for (i = 0; i < keys.length; i++) {
+        obj[keys[i]] = after[keys[i]];
+    }
+    return obj;
+}
