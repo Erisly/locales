306f68f99fc619cccad432eaf9de86730869ce5c
PikaDude <mail@pikadude.me>
2019-04-07 18:08:53 +1000

    Updated cron.sh

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "Updating..."
+git reset --hard
 git pull
 npm run data
 git add data.json
 git commit -m "Updated data.json"
-git add --all
-git commit -m "Inserted placeholder strings."
 git push
