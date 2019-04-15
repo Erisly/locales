21924f7ea05c9aa829a704ba9221db83b80e90f1
PikaDude <mail@pikadude.me>
2019-04-15 14:34:42 +1000

    Changes script.

---
+++ scripts/changes.js
@@ -0,0 +1,123 @@
+var async = require('async');
+var csv = require("fast-csv");
+var request = require('request');
+var config = require('./config.json');
+
+var oldChangesJSON = [];
+
+function main() {
+    var changesJSON = [];
+    var changesJSONProtected = [];
+    request('https://translate.pikagirl.me/changes/csv', function (err, res, body) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        if (res.statusCode == 200) {
+            csv.fromString(body, {
+                headers: true
+            }).on("data", function (data) {
+                changesJSON.push(data);
+                changesJSONProtected.push(data);
+            }).on("end", function () {
+                request('https://translate.pikagirl.me/api/components/PikaGirl/bot/statistics/', function (err, res, statistics) {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    if (res.statusCode != 200) {
+                        console.log(statistics);
+                        return;
+                    }
+                    try {
+                        var statisticsJSON = JSON.parse(statistics);
+                    } catch (Exception) {
+                        console.log(Exception);
+                        return;
+                    }
+
+                    if (oldChangesJSON.length == 0) {
+                        oldChangesJSON = changesJSONProtected;
+                        return;
+                    }
+
+                    async.each(oldChangesJSON, (oldChange, callback) => {
+                        var filter = changesJSON.filter(c => c.timestamp == oldChange.timestamp);
+
+                        if (filter.length != 0) {
+                            async.each(filter, (filterChange, callback2) => {
+                                changesJSON.splice(changesJSON.indexOf(filterChange), 1);
+                                callback2();
+                            }, function () {
+                                callback();
+                            });
+                        }
+                    }, function () {
+                        changesJSON.sort(function (a, b) {
+                            return new Date(a.timestamp) - new Date(b.timestamp);
+                        });
+
+                        async.each(changesJSON, (change, callback) => {
+                            var embedData = {
+                                embeds: [{
+                                    title: change.action,
+                                    url: change.url,
+                                    color: 3373751,
+                                    timestamp: change.timestamp,
+                                    fields: []
+                                }]
+                            };
+
+                            if (change.user != '') {
+                                embedData.embeds[0].fields.push({
+                                    name: 'User',
+                                    value: change.user,
+                                    inline: true
+                                });
+                            } else {
+                                embedData.embeds[0].fields.push({
+                                    name: 'User',
+                                    value: 'None',
+                                    inline: true
+                                });
+                            }
+                            if (change.target != '') {
+                                embedData.embeds[0].fields.push({
+                                    name: 'Detail',
+                                    value: change.target,
+                                    inline: true
+                                });
+                            }
+                            if (change.url.split('/bot/')[1] != '') {
+                                var language = statisticsJSON.results.filter(s => s.code == change.url.split('/bot/')[1].split('/')[0])[0];
+                                embedData.embeds[0].fields.push({
+                                    name: 'Language',
+                                    value: '**' + language.name + '** (' + language.translated_percent + '% Translated)',
+                                    inline: true
+                                });
+                            }
+                            request.post({
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                url: config.webhook,
+                                body: JSON.stringify(embedData)
+                            }, function (err, res, body) {
+                                if (err) console.log(err);
+                                if (res.statusCode != 200) console.log(body);
+
+                                setTimeout(callback, 1500);
+                            });
+                        }, function () {
+                            oldChangesJSON = changesJSONProtected;
+                        });
+                    });
+                });
+            });
+        } else console.log('Request responded with status code: ' + res.statusCode);
+    });
+}
+
+setInterval(main, config.interval);
+main();
